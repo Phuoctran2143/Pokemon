0c1812ab027f36245710e3f68ba97428cc787721
Phuoctran2143 <ptra1705@740s-pl-122.local>
2016-12-13 09:47:11 -0700

    added stuff to controller and runner

--- src/poke/controller/PokemonController.java
+++ src/poke/controller/PokemonController.java
@@ -1,5 +1,26 @@
 package poke.controller;
 
-public class PokemonController {
+import poke.model.*;
+import poke.view.PokemonFrame;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
+public class PokemonController 
+{
+	private ArrayList<Pokemon> pokedex;
+	private PokemonFrame appFrame;
+	
+	public void start() 
+	{
+		
+		
+	}
+	
+	public PokemonController()
+	{
+		pokedex = new ArrayList<Pokemon>();
+		buildPokedex();
+		
+		appFrame = new PokemonFrame(this);
+	}
 }
